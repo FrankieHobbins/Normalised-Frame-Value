2e99a600bcf488b01c1cbc1fec490a571d80949c
FrankieHobbins <acepainkiller@gmail.com>
2018-11-13 11:27:31 +0000

    added none check for active selected object

--- normalisedframe.py
+++ normalisedframe.py
@@ -14,15 +14,27 @@ bl_info = {
     "category": "3d View"}
 
 import bpy                                      #needed in a script-text-window!
-
+def fget(self):                                 # custom get function
+    #normframe = (bpy.context.scene.frame_current-bpy.context.scene.frame_start) / ( bpy.context.scene.frame_end - bpy.context.scene.frame_start) # normalised
+    normframe = (bpy.context.scene.frame_current) / ( bpy.context.scene.frame_end) # normalised
+    return normframe                             # return value
+bpy.types.Object.distance = 0
+bpy.types.Object.distance = property(fget)      # assign function to property, dont know if it should be byp types object but was in the example I grabbed and it seems to work
+ 
 class NormalisedFrameValue(bpy.types.Panel):                 # panel to display new property
     bl_space_type = "VIEW_3D"                   # show up in: 3d-window
     bl_region_type = "UI"                       # show up in: properties panel
     bl_label = "Normalised Frame Value"         # name of the new panel
-
+ 
     def draw(self, context):
-        self.layout.label(text=str((bpy.context.scene.frame_current) / ( bpy.context.scene.frame_end)))
-
+        # display "distance" of the active object
+        if bpy.context.active_object != None:
+            if bpy.context.selected_objects == []:
+                bpy.types.Object.distance = 0
+            else:
+                bpy.types.Object.distance = property(fget)
+            self.layout.label(text=str(bpy.context.active_object.distance))
+ 
 #bpy.utils.register_class(NormalisedFrameValue)               # register panel
 
 def register():
